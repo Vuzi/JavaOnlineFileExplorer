eff48df65828745e2c9677c68e8c69fde719fa92
Dimitri75 <bdimitri75@gmail.com>
2015-05-14 22:16:14 +0200

    Ajout Header, Footer Velocity
    Ajout actions header footer et fichiers vm

--- WEB-INF/src/fr/vuzi/fileexplorer/api/action/front/ActionGetResourceHeader.java
+++ WEB-INF/src/fr/vuzi/fileexplorer/api/action/front/ActionGetResourceHeader.java
@@ -1,27 +1,24 @@
 package fr.vuzi.fileexplorer.api.action.front;
 
-import fr.vuzi.fileexplorer.message.GenericMessage;
 import fr.vuzi.webframework.action.AActionNoCredentials;
 import fr.vuzi.webframework.action.IAction;
 import fr.vuzi.webframework.context.IContext;
 
-public class ActionFileExplorer extends AActionNoCredentials {
-
-	@Override
-	public IAction cloneAction(IContext context) {
-		ActionFileExplorer action = new ActionFileExplorer();
-		action.setActionContext(context);
-		return action;
-	}
-
+public class ActionGetResourceHeader extends AActionNoCredentials {
 	@Override
 	public int getPriority() {
 		return 0;
 	}
 
 	@Override
+	public IAction cloneAction(IContext context) {
+		IAction action = new ActionGetResourceHeader();
+		action.setActionContext(context);
+
+		return action;
+	}
+
+	@Override
 	public void proceed() throws Exception {
-		IContext c = getActionContext();
-		c.setAttribute("model", new GenericMessage(c.getParameterUnique("path")));
 	}
 }
